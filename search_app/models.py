46801b31a3cf70d3083e4de27c35d013ada1a42d
Chris <ntashi.x3@gmail.com>
2018-08-28 20:07:23 -0300

    remove blog model

--- search_app/models.py
+++ search_app/models.py
@@ -2,15 +2,6 @@ from django.contrib.postgres.search import SearchVectorField
 from django.db import models
 
 
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
-    lang = models.CharField(max_length=100, default="english")
-
-    def __str__(self):
-        return self.name
-
-
 class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
